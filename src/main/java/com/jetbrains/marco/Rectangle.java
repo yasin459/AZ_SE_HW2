b2a8400d0c1b2c7071023a2fa14aec4e784aa472
yasin <yasinnmoosavi@gmail.com>
2023-07-16 00:24:07 +0430

    add test to all clases

--- src/main/java/com/jetbrains/marco/Rectangle.java
+++ src/main/java/com/jetbrains/marco/Rectangle.java
@@ -1,7 +1,50 @@
 package com.jetbrains.marco;
 
-// class Rectangle {
+class Rectangle extends Shape {
 
+    public Rectangle(int height, int width){
+        super(height, width);
+    }
+    
+    
+}
+
+class Square extends Shape {
+
+    public Square(int height, int width){
+        super(height, width);
+    }
     
 
-// }
+}
+
+class Shape{
+
+    private int h;
+    private int w;
+
+    public Shape(int height, int width) {
+        h = height;
+        w = width;
+    }
+
+    public int getHeight() {
+        return h;
+    }
+
+    public int getWidth() {
+        return w;
+    }
+
+    public void  setHeight(int height) {
+        this.h = height;
+    }
+    
+    public void setWidthRectangle(int width) {
+        this.w = width;
+    }
+
+    public int computeArea(){
+        return h * w;
+    }
+}
--- src/test/java/com/jetbrains/marco/RectangleTest.java
+++ src/test/java/com/jetbrains/marco/RectangleTest.java
@@ -3,52 +3,79 @@ package com.jetbrains.marco;
 import org.junit.jupiter.api.*;
 
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-
 
 class RectangleTest {
     
     @Test
     void test_rectangle(){
-        Rectangle rect = new Rectangle();
+        Rectangle rect = new Rectangle(10,12);
+        assertNotNull(rect);
+    }
+
+    @Test
+    void test_compute_area(){
+        int h =10;
+        int w =12;
+        Rectangle rect = new Rectangle(h,w);
+        assertEquals(h*w, rect.computeArea());
+    }
+
+    @Test
+    void test_getter_setter_height(){
+        Rectangle rect = new Rectangle(5,7);
+        rect.setHeight(25);
+        int h = rect.getHeight();
+        rect.setHeight(h+6);
+        assertEquals(31 , rect.getHeight());
+    }
+    @Test
+    void test_getter_setter_width(){
+        Rectangle rect = new Rectangle(5,7);
+        rect.setWidthRectangle(25);
+        int h = rect.getWidth();
+        rect.setWidthRectangle(h+6);
+        assertEquals(31 , rect.getWidth());
+    }
+
+
+    @Test
+    void test_shape(){
+        Shape rect = new Shape(10,12);
+        assertNotNull(rect);
+    }
+
+    @Test
+    void test_compute_area_shape(){
+        int h =10;
+        int w =12;
+        Shape rect = new Shape(h,w);
+        assertEquals(h*w, rect.computeArea());
+    }
+
+    @Test
+    void test_getter_setter_height_shape(){
+        Shape rect = new Shape(5,7);
+        rect.setHeight(25);
+        int h = rect.getHeight();
+        rect.setHeight(h+6);
+        assertEquals(31 , rect.getHeight());
+    }
+    @Test
+    void test_getter_setter_width_shape(){
+        Shape rect = new Shape(5,7);
+        rect.setWidthRectangle(25);
+        int h = rect.getWidth();
+        rect.setWidthRectangle(h+6);
+        assertEquals(31 , rect.getWidth());
+    }
+
+    @Test
+    void test_square(){
+        Square rect = new Square(12,5);
         assertNotNull(rect);
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // @Test
-    // void test_getter_setter_height(){
-    //     mocked_rect.setHeight(25);
-    //     int h = mocked_rect.getHeight();
-    //     mocked_rect.setHeight(h+6);
-    //     assertEquals(31 , mocked_rect.getHeight());
-    // }
-    // @Test
-    // void test_getter_setter_width(){
-    //     mocked_rect.setWidthRectangle(25);
-    //     int h = mocked_rect.getWidth();
-    //     mocked_rect.setWidthRectangle(h+6);
-    //     assertEquals(31 , mocked_rect.getWidth());
-    // }
