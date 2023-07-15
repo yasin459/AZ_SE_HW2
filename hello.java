e4a9dae92df6bcbff3517e39755e6c5dc730a275
yasin <yasinnmoosavi@gmail.com>
2023-07-15 20:32:28 +0430

    change hieght and width of rectangle

--- hello.java
+++ hello.java
@@ -6,7 +6,21 @@ class Rectangle {
         h = height;
         w = width;
     }
+    public int getHeight() {
+        return h;
+    }
 
+    public int getWidth() {
+        return w;
+    }
+
+    public void  setHeight(int height) {
+        this.h = height;
+    }
+    
+    public void setWidthRectangle(int width) {
+        this.w = width;
+    }
     public int computeArea() {
         return h * w;
     }
@@ -16,5 +30,9 @@ class HelloWorld {
     public static void main(String[] args) {
         Rectangle rect = new Rectangle(10, 20);
         System.out.println(rect.computeArea()); 
+        rect.setHeight((12));
+        rect.setWidthRectangle(7);
+        System.out.println(rect.computeArea());
+
     }
 }
