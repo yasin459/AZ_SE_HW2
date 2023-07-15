ead562f2d4f7aa35c6294960c18f4ce39c3b0322
yasin <yasinnmoosavi@gmail.com>
2023-07-15 22:05:11 +0430

    start unit testing for rectangle

---
+++ src/main/java/com/jetbrains/marco/Rectangle.java
@@ -0,0 +1,12 @@
+package com.jetbrains.marco;
+
+class Rectangle {
+    private int h;
+    private int w;
+
+    public Rectangle(int height, int width) {
+        h = height;
+        w = width;
+    }
+
+}
